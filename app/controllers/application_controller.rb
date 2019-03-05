a2119aa3863794d52c411f5f970ae86fa1f23bfc
Robert McCary <robert.mccary@yahoo.com>
2019-03-05 16:58:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,11 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  # post '/login' do
-  #   @user=User.find_by(username: params[:username], password: params["password"])
-  #   session[:user_id] = @user.id
-  #   redirect to '/account'
-  # end
+  post '/login' do
+    @user=User.find_by(username: params[:username], password: params["password"]
+    session[:user_id] = @user.id
+    redirect to '/account'
+  end
 
   get '/account' do
 
